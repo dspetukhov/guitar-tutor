958116293da6764dfdc42eb4f1909cf4ce953ab3
dspetukhov <dspetukhov.work@gmail.com>
2026-06-21 11:28:19 +0400

    fix: revised & tested for harmony lane

--- transcriber/transcribe-tuned.py
+++ transcriber/transcribe-tuned.py
@@ -16,25 +16,24 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
-N_TRIALS = 260
+N_TRIALS = 26 * 3
 ARTIFACTS_DIR = "artifacts"
 LOGS_DIR = "logs"
 
-HARMONY_CRITERIA = {
+CRITERIA_DIRECTION = {
+    # Harmony
     "RMSE": "minimize",
     "Cosine similarity": "maximize",
     "L2 Distance": "maximize",
     "Energy coverage": "maximize",
+    # Melody
+    "F1": "maximize",
+    "Salience": "maximize",
 }
 HARMONY_FEATURES = {
     "STFT": "minimize",
     "CQT": "maximize",
     "HCQT": "maximize",
-}
-
-MELODY_CRITERIA = {
-    "F1": "maximize",
-    "Salience": "maximize",
 }
 
 
@@ -43,39 +42,39 @@ def get_params_key(params: dict) -> str:
     return hashlib.md5(json.dumps(params, sort_keys=True).encode()).hexdigest()
 
 
-def harmony_objective(trial, waveform, sample_rate, extractor, metric, cache, TT):
+def harmony_objective(
+    trial, waveform, sample_rate, extractor, criterion, cache, TT, default_return
+):
     """
-    Optuna objective that tunes transformation parameters.
+    Optuna objective that tunes extractor parameters.
 
     To Do:
-    - beat-synchronous chroma as an alternative to framewise (current)
+    - beat-synchronous chroma as an alternative to frame-wise (current)
     - [Optional] HPSS before Chroma to isolate harmonic content (reduces drum bleed) in live recordings
     - [Optional] temporal smoothing with median filter or HMM/Viterbi to reduce jitter
     """
     n_chroma = 12
 
     params = suggest_parameters(trial, extractor, n_chroma=n_chroma)
-    if params is None:
-        return np.inf
+    if not params:
+        return default_return
     key = get_params_key(params)
     if key in cache:
-        return cache[key]["evals"][metric]
+        return cache[key]
 
     try:
-        features = extract_features(
-            extractor, waveform, sample_rate, n_chroma, **params
-        )
+        features = extract_features(extractor, waveform, sample_rate, n_chroma, params)
     except librosa.util.exceptions.ParameterError:
-        logging.warning("Trial raised ParameterError; scoring as +inf")
-        return np.inf
+        logging.warning(f"Trial raised ParameterError; scoring as {default_return}")
+        return default_return
 
-    logging.info(f"Features [{extractor}]: {features.shape}")
+    logging.info(f"Features [{extractor} | {criterion}]: {features.shape}")
 
     scores = TT @ features
     predictions = np.argmax(scores, axis=0)
     evals = evaluate_harmony(features, TT, predictions)
-    cache[key] = {"params": params, "evals": evals}
-    return cache[key]["evals"][metric]
+    cache[key] = evals[criterion]
+    return cache[key]
 
 
 def melody_objective(trial, waveform, sample_rate, metric, cache):
@@ -126,16 +125,13 @@ def melody_objective(trial, waveform, sample_rate, metric, cache):
     return cache[key]["evals"][metric]
 
 
-def run_study(study_type: str, method: str, criterion: str, direction: str, T) -> dict:
-    if study_type == "melody":
-        cache = {}
-        study_name = f"{study_type}-{criterion}"
-    else:
-        cache = {"method": method}
-        study_name = f"{study_type}-{method}-{criterion}"
+def run_study(study_type: str, extractor: str, criterion: str, TT) -> dict:
+    study_name = f"{study_type}-{extractor}-{criterion}".lower().replace(" ", "-")
     storage = optuna.storages.JournalStorage(
         optuna.storages.journal.JournalFileBackend(f"{LOGS_DIR}/{study_name}.log")
     )
+    direction = CRITERIA_DIRECTION[criterion]
+
     study = optuna.create_study(
         study_name=study_name,
         direction=direction,
@@ -145,6 +141,7 @@ def run_study(study_type: str, method: str, criterion: str, direction: str, T) -
     )
 
     completed = 0
+    cache = {}
     for trial in study.get_trials(states=[optuna.trial.TrialState.COMPLETE]):
         cache[get_params_key(trial.params)] = trial.value
         completed += 1
@@ -152,44 +149,66 @@ def run_study(study_type: str, method: str, criterion: str, direction: str, T) -
     remaining = N_TRIALS - completed
     if completed:
         logging.info(
-            f"{completed}/{N_TRIALS} trials already done for {method}/{criterion},"
+            f"{completed}/{N_TRIALS} trials already done for {extractor}/{criterion},"
             f"running {remaining} more"
         )
-
-    if study_type == "harmony":
-        study.optimize(
-            lambda trial: harmony_objective(
-                trial, waveform, sample_rate, criterion, cache, T
-            ),
-            n_trials=remaining,
-        )
-    elif study_type == "melody":
-        study.optimize(
-            lambda trial: melody_objective(
-                trial, waveform, sample_rate, criterion, cache
-            ),
-            n_trials=remaining,
-        )
-    else:
-        logging.warning("Unknow study type")
-        return {None: None}
+    default_return = np.inf if direction == "minimize" else -np.inf
+    # if study_type == "harmony":
+    study.optimize(
+        lambda trial: harmony_objective(
+            trial,
+            waveform,
+            sample_rate,
+            extractor,
+            criterion,
+            cache,
+            TT,
+            default_return,
+        ),
+        n_trials=remaining,
+    )
+    # elif study_type == "melody":
+    #     study.optimize(
+    #         lambda trial: melody_objective(
+    #             trial, waveform, sample_rate, criterion, cache
+    #         ),
+    #         n_trials=remaining,
+    #     )
+    # else:
+    #     logging.warning("Unknow study type")
+    #     return {None: None}
 
     best_trial = {
-        "number": study.best_trial.number,
+        # "number": study.best_trial.number,
         "value": study.best_trial.value,
         "params": study.best_trial.params,
     }
-
+    # print(cache)
     return best_trial
 
 
+def load_config(file_path):
+    if file_path.is_file():
+        with open(file_path, encoding="utf-8") as file:
+            config = json.load(file)
+            logging.info(f"Configuration loaded: {file_path}")
+            return config
+    else:
+        raise SystemExit("Exit: configuration file wasn't found")
+
+
 if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        config = load_config(Path(sys.argv[1]))
+    else:
+        raise SystemExit("Usage: python main.py <config_file_path>")
+
     Path(ARTIFACTS_DIR).mkdir(parents=True, exist_ok=True)
     Path(LOGS_DIR).mkdir(parents=True, exist_ok=True)
 
     TT, _ = triads_template()
 
-    audio_file_path = Path("ll-slice.wav")
+    audio_file_path = Path(config["audio_file"])
     if audio_file_path.is_file():
         waveform, sample_rate = librosa.load(
             audio_file_path.as_posix(),
@@ -198,25 +217,24 @@ if __name__ == "__main__":
         )
         logging.info(f"Waveform: {waveform.shape} | Sample rate: {sample_rate:,}")
     else:
-        sys.exit(1)
+        raise SystemExit("Exit: audio file wasn't found")
 
-    # harmony_criterion, direction = "RMSE", "minimize"
-    # stft = run_study("harmony", "stft", harmony_criterion, direction, T)
-    # logging.info(f"stft: {stft}")
-    # cqt = run_study("harmony", "cqt", harmony_criterion, direction, TT)
-    # logging.info(f"cqt: {cqt}")
+    output = {"audio_file": config["audio_file"]}
+    if config.get("harmony_lane"):
+        output["harmony_lane"] = []
+        for item in config["harmony_lane"]:
+            extractor = item["extractor"]
+            criterion = item["criterion"]
+            result = run_study("harmony", extractor, criterion, TT)
+            output["harmony_lane"].append(
+                {
+                    "extractor": extractor,
+                    "criterion": criterion,
+                    "value": result["value"],
+                    "parameters": result["params"],
+                    "hash": get_params_key(result["params"]),
+                }
+            )
 
-    # melody_criterion, direction = "salience", "maximize"
-    # melody = run_study("melody", "", melody_criterion, direction, T)
-    # logging.info(f"melody: {melody}")
-
-    # output = {
-    #     "audio_file_path": str(audio_file_path),
-    #     "harmony-criterion": harmony_criterion,
-    #     "harmony-stft": stft,
-    #     "harmony-cqt": cqt,
-    # "melody-criterion": melody_criterion,
-    # "melody": melody
-    # }
-    # with (Path(ARTIFACTS_DIR) / audio_file_path.stem).open("w") as file:
-    #     json.dump(output, file, indent=4)
+    with (Path(ARTIFACTS_DIR) / audio_file_path.stem).open("w") as file:
+        json.dump(output, file, indent=4)
